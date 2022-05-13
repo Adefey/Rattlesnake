a73b6b1c38a22790edfe6ce13f15f20721659854
lonkidely <inkandies.social@gmail.com>
2022-05-13 13:32:26 +0300

    Fix parameters in data_processing lib

--- libs/data_processing/include/parser.hpp
+++ libs/data_processing/include/parser.hpp
@@ -9,8 +9,8 @@ class Parser {
  public:
   static bool ToInt(const std::string& value, int& result);
   static bool ToDouble(const std::string& value, double& result);
-  static bool ParseBlockFromJsonString(std::string val, Block& result);
-  static bool ParseBlocksFromJsonString(std::string val, std::vector<Block>& result);
+  static bool ParseBlockFromJsonString(const std::string& val, Block& result);
+  static bool ParseBlocksFromJsonString(const std::string& val, std::vector<Block>& result);
   static bool ParseParameterFromJsonString(const std::string& val, Parameter& result);
-  static bool ParseParametersFromJsonString(std::string val, std::vector<Parameter>& result);
+  static bool ParseParametersFromJsonString(const std::string& val, std::vector<Parameter>& result);
 };
