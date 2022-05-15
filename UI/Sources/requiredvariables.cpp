eca6094ad1e3e11f28937f960f6c0dc0d547f2b5
nokrolikno <vasya.ermakov.2001@mail.ru>
2022-05-15 10:48:06 +0000

    UI MVP

--- UI/Sources/requiredvariables.cpp
+++ UI/Sources/requiredvariables.cpp
@@ -28,6 +28,7 @@ void RequiredVariables::updateTable(std::vector<Block*> *blocks) {
             tableItem1->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
             table->setItem(n_rows, 0, tableItem1);
             QComboBox *comboBox = new QComboBox;
+            comboBox->setEditable(true);
             for (size_t k = 0; k < i; ++k) {
                 for (size_t w = 0; w < ((*blocks)[k])->GetSolvedVars().size(); ++w)
                 comboBox->insertItem(comboBox->count(), QString::fromStdString(
@@ -50,8 +51,7 @@ void RequiredVariables::run() {
     }
     std::vector<std::string> values;
     for (size_t i = 0; i < n_rows; ++i) {
-        values.push_back((qobject_cast<QComboBox*>(table->cellWidget(i, 1))->
-                         itemData(qobject_cast<QComboBox*>(table->cellWidget(i, 1))->currentIndex())).toString().toStdString());
+        values.push_back(qobject_cast<QComboBox*>(table->cellWidget(i, 1))->currentText().toStdString());
     }
     std::vector<Parameter> parameters;
     for (size_t i = 0; i < names.size(); ++i) {
